e32e144a3fe31224496f3391821064fe368103c3
Snehangshu Bhattacharya <snehangshub1.618@gmail.com>
2024-09-30 22:50:46 +0530

    Fixed video width issue in cpu

--- helpers/video.py
+++ helpers/video.py
@@ -8,4 +8,5 @@ def resize_video(self, input_file: str, output_file: str, height: int, cuda=Fals
         ffmpeg.input(input_file, hwaccel='cuda').output(output_file, vf=f'hwupload_cuda,scale_cuda=w=-1:h={height}',
                                                         vcodec='h264_nvenc').overwrite_output().run()
     else:
-        ffmpeg.input(input_file).filter('scale', 'trunc(iw/2)*2', height).output(output_file).overwrite_output().run()
+        # CPU encoder needs width divisible by 2, otherwise it will fail
+        ffmpeg.input(input_file).filter('scale', 'trunc(oh * (iw/ih)/2) * 2', height).output(output_file).overwrite_output().run()

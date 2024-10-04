fa7b39d5471499cec75b18dc2c8e702a4ef6ca15
Snehangshu Bhattacharya <snehangshub1.618@gmail.com>
2024-10-04 15:50:28 +0530

    cosmetic changes

--- celery_worker.py
+++ celery_worker.py
@@ -1,8 +1,6 @@
 from celery import Celery
 
-app = Celery('hello', backend='redis://localhost:6379/0', broker='amqp://guest@localhost//', include=['helpers.video'])
-app.conf.update(task_ignore_result=False) # result_backend='redis://localhost:6379/0',
-app.autodiscover_tasks()
+app = Celery('celery-video-transcoder', backend='redis://localhost:6379/0', broker='amqp://guest@localhost//', include=['helpers.video'])
 
 
 @app.task
